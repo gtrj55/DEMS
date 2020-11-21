5ac75c197ea153abc93776230e3a241f7d55314b
gtrj55 <gtrj55@gmail.com>
2020-11-21 11:27:56 +0530

    Add files via upload

---
+++ lib/webparts/searchWp/components/SPServices/SPOperation.d.ts
@@ -0,0 +1,7 @@
+import { WebPartContext } from '@microsoft/sp-webpart-base';
+export declare class SPOperation {
+    protected onInit(context: WebPartContext): void;
+    getSector(context: WebPartContext): Promise<any>;
+    GetSkill(context: WebPartContext): Promise<any>;
+}
+//# sourceMappingURL=SPOperation.d.ts.map
